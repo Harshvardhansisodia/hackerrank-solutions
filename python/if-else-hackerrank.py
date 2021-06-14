471b5459332f25484331a40a7b019c4be37e0471
Harshul <74238454+Harshvardhansisodia@users.noreply.github.com>
2021-06-14 17:43:24 +0530

    Add files via upload

---
+++ python/if-else-hackerrank.py
@@ -0,0 +1,13 @@
+n=int(input())
+if n%2==1:
+    print("Weird")
+else:
+    if 2<n<5:
+     print("Not Weird")
+    else:
+        if 6<n<=20:
+            print("Weird")
+        else:
+            if n>20:
+                print("Not Weird")
+
